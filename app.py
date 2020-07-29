565d572a3e758241173d0a525fb00e702c68a155
andreamartz <14165788+andreamartz@users.noreply.github.com>
2020-07-29 09:00:38 -0500

    Add route for survey questions

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, redirect
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "abc123"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
+# ********************************************
+# GLOBAL VARIABLES
+# responses -> initialize responses; this is the variable that will store survey answers
+#     e.g. responses = ['Yes', 'No', 'Less than $10,000', 'Yes']
+responses = []
+# survey -> stores the survey questions and choices
+survey = satisfaction_survey
+# *********************************************
+
+
+@app.route('/')
+def home_page():
+    """Shows survey start page"""
+    return render_template('home.html', survey=survey)
+
+
+@app.route('/questions/<int:idx>')
+def show_question(idx):
+    """Shows question from the survey"""
+    question = survey.questions[idx].question
+    choices = survey.questions[idx].choices
+    return render_template('question.html', survey=survey, question=question, choices=choices, idx=idx)
